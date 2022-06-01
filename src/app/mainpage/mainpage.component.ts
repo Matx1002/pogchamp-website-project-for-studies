30a9ff4849ede3fef5c5d4dea07a6c2f41126323
m <mateusz.wa2001@gmail.com>
2022-06-01 21:45:51 +0200

    feature update

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import getDocumentElement from "@popperjs/core/lib/dom-utils/getDocumentElement";
 
 @Component({
   selector: 'app-mainpage',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainpageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    document.getElementById("przycisk")?.addEventListener('click', () => {
+      let form = document.getElementById("formularzyk");
+      if(!form) return;
+      var headerOffset = 100;
+      var elementPosition = form.getBoundingClientRect().top;
+      var offsetPosition = elementPosition+window.pageYOffset - headerOffset;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+      });
+    })
   }
 
 }
